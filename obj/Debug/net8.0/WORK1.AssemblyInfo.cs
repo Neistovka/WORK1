a57bdc89ef5364682cfba29d1e37485467d53f20
Neistovka <147620315+Neistovka@users.noreply.github.com>
2024-06-17 23:51:03 +0700

    obj

--- obj/Debug/net8.0/WORK1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WORK1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WORK1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ae90d215845d38e8feee9b40a906c83c4ebd64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5277b324f2c229d4311f6cd8df329efb1700a18f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WORK1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WORK1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
